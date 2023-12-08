87fc8c495d0bddb41596943693ed54a28505d21f
DiegoBot <151096440+diegojadibot@users.noreply.github.com>
2023-12-08 13:03:25 -0500

    Actualizar _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `🌺FAɴᴛᴀsʏBᴏᴛ Uso:Pᴜʙʟɪᴄᴏ • 🔮 ${uptime} | ☃️ ${global.packname} ☃️ ⒸⒹ para ver el menu usa .menu | 𝐛𝐲 diego`
+let bio = `🌺FAɴᴛᴀsʏBᴏᴛ Uso:Pᴜʙʟɪᴄᴏ ᯾✅ • 🔮 ${uptime} | ☃️ ${global.packname} ☃️ ⒸⒹ para ver el menu usa .menu | 𝐛𝐲 diego`
 await this.updateProfileStatus(bio).catch(_ => _)
 setting.status = new Date() * 1
 } 
