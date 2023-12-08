6a29c1c974f52dd4e94fffbcc65690f139368980
DiegoBot <151096440+diegojadibot@users.noreply.github.com>
2023-12-08 10:17:37 -0500

    Actualizar update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
-# Código desarrollado por @gata_dios 
+# Código desarrollado por @gata:dios
   
 #!/data/data/com.termux/files/usr/bin/bash 
 BOT_DIR="FantasyBot-MD"  
-BOT_REPO="https://github.com/Wilsmac/$BOT_DIR"
+BOT_REPO="https://github.com/diegojadiboy/$BOT_DIR"
 DB_FILE="database.json"
  
 GREEN='\033[32m'
